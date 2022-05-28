e03108a441eb066d1b02f6c758d86ad117b25c62
GustavoRosauro <gustavosrosauro@gmail.com>
2022-05-28 20:36:48 -0300

    executando mais de uma ação

--- chrome.py
+++ chrome.py
@@ -6,13 +6,17 @@ opts = Options()
 
 driver = Chrome(options=opts, executable_path='chromedriver.exe')
 try:
-    driver.get('https://portal.qcertifica.com.br/Authentication/Login.aspx?enc=IuazGqDuFUuv0DlQ9kwRLn8grINIeo9xG89TdjTBfbo=')
+    driver.get('https://www.quicksoft.com.br/')
+    tagA = driver.find_element_by_link_text('Acesso ao Cliente')
+    tagA.send_keys(Keys.ENTER)
+    driver.get('https://portal.qcertifica.com.br/Authentication/Login.aspx')
     login = driver.find_element_by_name('ctl00$body$txtUsuario')
     senha = driver.find_element_by_name('ctl00$body$txtSenha')
-    login.send_keys('1')
-    senha.send_keys('2')
+    login.send_keys(
+        'eu sou um robo em python falem com o Arquiteto Gustavo Rosauro')
+    senha.send_keys('realizando testes automatizados')
     senha.send_keys(Keys.ENTER)
 
 finally:
     print('acabou')
-    # driver.quit()
+    #driver.quit()
